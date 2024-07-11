d610b6ee9d31f342fbb27b1bde25449595ae5d49
grizeus <zaytsewww@gmail.com>
2024-07-11 12:54:14 +0300

    add tasks, pretify html a bit

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,25 @@
 "use strict";
 
-function getElementWidth(content, padding, border) {
-  return Number.parseFloat(content) + (2 * Number.parseFloat(padding)) + (2 * Number.parseFloat(border));
+function getShippingCost(country) {
+
+  const fees = [100, 250, 170, 120];
+  switch (country) {
+    case "China":
+      return `Shipping to ${country} will cost ${fees[0]} credits`
+    case "Chile":
+      return `Shipping to ${country} will cost ${fees[1]} credits`
+    case "Australia":
+      return `Shipping to ${country} will cost ${fees[2]} credits`
+    case "Jamaica":
+      return `Shipping to ${country} will cost ${fees[3]} credits`
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
