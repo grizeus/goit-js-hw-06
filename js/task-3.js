364c52fbf52e2ff4610d0ce1cccda413c8a69cf2
grizeus <zaytsewww@gmail.com>
2024-07-12 08:24:34 +0300

    add last task, fix index

--- js/task-3.js
+++ js/task-3.js
@@ -1,28 +1,19 @@
 'use strict';
 
-function checkForSpam(message) {
+function filterArray(numbers, value) {
+  const result = [];
 
-  const blackList = ['spam', 'sale'];
-  const blackLen = blackList.length;
-  let result = false;
-  const splitted = message.split(" ");
-  const splitLen = splitted.length;
-
-  for (let i = 0; i < splitLen; i++) {
-    for (let j = 0; j < blackLen; j++) {
-      result = splitted[i].toLowerCase().includes(blackList[j]);
-      if (result === true) {
-        return result;
-      }
+  for (const number of numbers) {
+    if (number > value) {
+      result.push(number);
     }
   }
+
   return result;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
